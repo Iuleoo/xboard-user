d97edadac1ae4d48d032547f20f6f5a2a13b65e3
RJK <KlyinT@qq.com>
2024-10-11 22:33:20 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,9 +2,9 @@
 window.routerBase = 'https://vv.333387.xyz/'
 window.settings = {
   // 站点名称
-  title: 'Xboard',
+  title: '一树梨花压海棠',
   // 站点描述
-  description: 'Xboard',
+  description: 'Brother, Dont pickpocket my website!',
   assets_path: '/assets',
   // 主题色
   theme: {
@@ -13,7 +13,7 @@ window.settings = {
   // 版本号
   version: '0.1.1-dev',
   // 登陆背景
-  background_url: '',
+  background_url: 'https://f002.backblazeb2.com/file/choleimg/2024/01/27/8ec65bb8cd77e217139649b0743c05f4-1-comps.jpg',
   // 站点LOGO
   logo: '',
 }
